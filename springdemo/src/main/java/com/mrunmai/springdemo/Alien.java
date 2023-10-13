300a39019266310944a634fa5e214024dcdd94d7
mrunmai <mrunmaikudale18@gmail.com>
2023-10-13 20:29:59 +0530

    Spring container which stores an object and spring follows singleton design pattern

--- springdemo/src/main/java/com/mrunmai/springdemo/Alien.java
+++ springdemo/src/main/java/com/mrunmai/springdemo/Alien.java
@@ -1,8 +1,15 @@
 package com.mrunmai.springdemo;
 
 public class Alien {
+	
+	int age;
+	
+	public Alien() {
+		System.out.println("Alien object created...");
+	}
+
 	public void code() {
 		System.out.println("I am coding...");
-		
+
 	}
 }
--- springdemo/src/main/java/com/mrunmai/springdemo/App.java
+++ springdemo/src/main/java/com/mrunmai/springdemo/App.java
@@ -3,19 +3,29 @@ package com.mrunmai.springdemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class App {
+	public static void main(String[] args) {
+		// BeanFactory factory = new XmlBeanFactory(new
+		// FileSystemResource("spring.xml"));
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-    	
-        Alien obj = (Alien) context.getBean("alien");
-        obj.code();
-    }
+//    	Even if we don't call getBean(), spring will create the object.
+		Alien obj1 = (Alien) context.getBean("alien");
+		obj1.code();
+		obj1.age = 15;
+		System.out.println(obj1.age);
+
+		Alien obj2 = (Alien) context.getBean("alien");
+		obj2.code();
+
+		System.out.println(obj2.age);
+
+//		Beans are classes which has variables and methods.        
+//      The JVM has container and container has all the beans.
+//      Spring will create the bean even if you don't create it.
+		/*
+		 * The object is created only once, and every time you try create the new object
+		 * spring container will give the same object. This is called Singleton design pattern which means by default, spring will give you one object.
+		 */
+	}
 }
